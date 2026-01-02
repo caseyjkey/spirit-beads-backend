812b7800f3fbc73c6fe2df22e068dfa74743630f
Casey <hey@keycasey.com>
2026-01-02 06:57:51 +0000

    prod working

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ router.register(r'products', ProductViewSet)
 router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- spiritbead/urls.py
+++ spiritbead/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('api/', include('products.urls')),
     path('api/payments/', include('payments.urls')),
 ]
 
